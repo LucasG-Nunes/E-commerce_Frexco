da52189e6bfb809597474190af61b45b68f7f41e
LucasG-Nunes <lucasgnam12@gmail.com>
2022-08-08 13:17:23 -0300

    :wrench: Estruturando Navbar

--- ecommerce_frexco/src/Components/Navbar/Navbar.js
+++ ecommerce_frexco/src/Components/Navbar/Navbar.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='Navbar'>Navbar</div>
+    <div className='Navbar'>
+      <h1>LOGO</h1>
+      <div className='Itens'>
+        <span>NOME1</span>
+        <span>NOME2</span>
+        <span>NOME3</span>
+        <span>NOME4</span>
+      </div>
+    </div>
   )
 }
 
