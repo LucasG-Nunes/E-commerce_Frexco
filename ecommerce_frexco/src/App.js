334df9033c0500dceb76ca002c8fdb13c4333527
LucasG-Nunes <lucasgnam12@gmail.com>
2022-08-07 21:11:17 -0300

    :rocket: Chamando o  primeiro componente

--- ecommerce_frexco/src/App.js
+++ ecommerce_frexco/src/App.js
@@ -1,8 +1,12 @@
-
+import { useAxios } from "./Hooks/useAxios";
+import Navbar from "./Components/Navbar/Navbar";
 function App() {
+  const {data} = useAxios();
+  console.log(data)
   return (
     <div className="App">
      <h2>Iniciando o desenvolvimento</h2>
+     <Navbar/>
     </div>
   );
 }
