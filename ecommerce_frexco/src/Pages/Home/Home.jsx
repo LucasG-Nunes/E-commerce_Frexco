9186d392ef93b9c56d3640d45a2f2d15b36a68f8
LucasG-Nunes <lucasgnam12@gmail.com>
2022-08-08 13:14:33 -0300

    :construction: Estruturando primeira section

--- ecommerce_frexco/src/Pages/Home/Home.jsx
+++ ecommerce_frexco/src/Pages/Home/Home.jsx
@@ -1,10 +1,18 @@
 import Navbar from "../../Components/Navbar/Navbar"
-
+import image from "../../Images/personFruits.png"
 const Home = () => {
   return (
     <div>
         <Navbar/>
-        
+        <div className="containerIntrodutorio">
+          <div className="text">
+            <span>teste</span>
+            <p>Lorem ipsum dolor etc</p>
+          </div>
+          <div className="image">
+            <img src={image} alt="personfood" />
+          </div>
+        </div>
     </div>
   )
 }
