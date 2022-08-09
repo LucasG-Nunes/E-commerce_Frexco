e830245c5894f91d2a53a59c44166c680cc18d53
LucasG-Nunes <lucasgnam12@gmail.com>
2022-08-09 14:27:09 -0300

    :construction: Desenvolvimento da lógica de visualização do botão confirmar compra e do modal. Caso a quantidade do produto seja maior que 0, ira aparecer o botão para confirmar compra. Ao clicar no botão, será setado um estado para true e irá mostrar o modal.

--- ecommerce_frexco/src/Pages/Carrinho/Carrinho.jsx
+++ ecommerce_frexco/src/Pages/Carrinho/Carrinho.jsx
@@ -3,53 +3,54 @@ import { useContext } from "react"
 import { CarrinhoContext } from "../../Context/CarrinhoContext"
 import { useAxiosParams } from "../../Hooks/useAxios"
 import { useState } from "react"
+import Modal from "../../Components/ModalCompra/Modal"
 
 const Carrinho = () => {
-  const { fruit,setFruit } = useContext(CarrinhoContext);
-  const {data} = useAxiosParams(fruit);
-  const [plus,setPlus] = useState(1)
+  const { fruit, setFruit } = useContext(CarrinhoContext);
+  const { data } = useAxiosParams(fruit);
+  const [plus, setPlus] = useState(1)
+  const [modal,setModal] = useState(false)
   return (
     <div className="background">
-      <Navbar/>
+      <Navbar />
       {!fruit && <p className="avisoVazio">Não há nada no seu carrinho!</p>}
-      {data ? 
-      <div className="containerCarrinho">
-        <div>
-          <img src={data.image} alt="ImagemFruta" />
-        </div>
-            <div className="Informations">
-              <div className="header">
+      {data ?
+        <div className={"containerCarrinho"}>
+          <div>
+            <img src={data.image} alt="ImagemFruta" />
+          </div>
+          <div className="Informations">
+            <div className="header">
               <h4>{data.name}</h4>
-              <button onClick={()=>{
+              <button onClick={() => {
                 setFruit('')
                 window.location.reload();
               }}>x</button>
-              </div>
-              <div className="nutritions">
+            </div>
+            <div className="nutritions">
               <span>Valor: ${data.price}</span>
               <span>Calorias: {data.nutritions.calories}</span>
               <span>Gordura: {data.nutritions.fat}</span>
               <span>Carboidrato: {data.nutritions.carbohydrates}</span>
               <span>Proteina:{data.nutritions.protein}</span>
               <span>Açucar: {data.nutritions.sugar}</span>
-              </div>
-                <span className="Quantidade">Quantidade: {plus}</span>
-              <div className="buttons">
+            </div>
+            <span className="Quantidade">Quantidade: {plus}</span>
+            <div className="buttons">
 
-              <button onClick={()=>{
+              <button onClick={() => {
                 setPlus(plus + 1)
               }}>+</button>
-              <button onClick={()=>{
-                setPlus(plus -1)
+              <button onClick={() => {
+                setPlus(plus - 1)
               }}>-</button>
-              </div>
-              <span className="valorTotal">Valor Total: $ {data.price * plus}</span>
             </div>
-
-
-
-    </div>
-      : null}
+            <span className="valorTotal">Valor Total: $ {data.price * plus}</span>
+          {plus > 0 ? <button className="confirmarCompra" onClick={()=>{setModal(true)}}>Confirmar Compra</button>: null}
+          {modal ? <Modal setModal={setModal}/> : null}
+          </div>
+        </div>
+        : null}
     </div>
   )
 }
