e8d64b82980ab644108309272110c56244dfb7bc
LucasG-Nunes <lucasgnam12@gmail.com>
2022-08-09 14:18:22 -0300

    :sparkles: Toques finais melhores praticas html para a navbar

--- ecommerce_frexco/src/Components/Navbar/Navbar.jsx
+++ ecommerce_frexco/src/Components/Navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import {InputContext} from "../../Context/InputContext"
 const Navbar = () => {
   const {setInput}= useContext(InputContext)
   return (
-    <div className='Navbar'>
+    <nav className='Navbar'>
       <h1>FREXCO</h1>
       <input type='text' onChange={(event)=>{setInput(event.target.value)}} />
       <div className='Itens'>
         <NavLink className="NavLink" to="/">Home</NavLink>
         <NavLink className="NavLink" to="/carrinho">Carrinho</NavLink>
       </div>
-    </div>
+    </nav>
   )
 }
 
