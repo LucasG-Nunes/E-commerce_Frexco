f9086b910e8a91bac6117672d7050e00dea8cf79
LucasG-Nunes <lucasgnam12@gmail.com>
2022-08-09 01:05:49 -0300

    :tada: Página home estruturada com navbar, container inicial e container das frutas

--- ecommerce_frexco/src/Pages/Home/Home.jsx
+++ ecommerce_frexco/src/Pages/Home/Home.jsx
@@ -1,18 +1,26 @@
+import DisplayProdutos from "../../Components/DisplayProdutos/DisplayProdutos"
 import Navbar from "../../Components/Navbar/Navbar"
 import image from "../../Images/personFruits.png"
 const Home = () => {
+
   return (
     <div>
         <Navbar/>
         <div className="containerIntrodutorio">
           <div className="text">
-            <span>teste</span>
-            <p>Lorem ipsum dolor etc</p>
+            <span>HortiFruit Ecommerce</span>
+            <p>uma vida melhor começa por uma alimentação saudável</p>
           </div>
           <div className="image">
             <img src={image} alt="personfood" />
           </div>
         </div>
+        <DisplayProdutos/>
+        <footer>
+          <p>
+            <span>Desenvolvido por Lucas Gabriel @ 2022</span>
+          </p>
+        </footer>
     </div>
   )
 }
