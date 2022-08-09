262a4d11b2bb84cff7c4d51a1b8c7410bee09de3
LucasG-Nunes <lucasgnam12@gmail.com>
2022-08-09 01:12:07 -0300

    :fire: Removendo comentarios do código.

---
+++ ecommerce_frexco/src/Pages/Carrinho/Carrinho.jsx
@@ -0,0 +1,57 @@
+import Navbar from "../../Components/Navbar/Navbar"
+import { useContext } from "react"
+import { CarrinhoContext } from "../../Context/CarrinhoContext"
+import { useAxiosParams } from "../../Hooks/useAxios"
+import { useState } from "react"
+
+const Carrinho = () => {
+  const { fruit,setFruit } = useContext(CarrinhoContext);
+  const {data} = useAxiosParams(fruit);
+  const [plus,setPlus] = useState(1)
+  return (
+    <div className="background">
+      <Navbar/>
+      {!fruit && <p className="avisoVazio">Não há nada no seu carrinho!</p>}
+      {data ? 
+      <div className="containerCarrinho">
+        <div>
+          <img src={data.image} alt="ImagemFruta" />
+        </div>
+            <div className="Informations">
+              <div className="header">
+              <h4>{data.name}</h4>
+              <button onClick={()=>{
+                setFruit('')
+                window.location.reload();
+              }}>x</button>
+              </div>
+              <div className="nutritions">
+              <span>Valor: ${data.price}</span>
+              <span>Calorias: {data.nutritions.calories}</span>
+              <span>Gordura: {data.nutritions.fat}</span>
+              <span>Carboidrato: {data.nutritions.carbohydrates}</span>
+              <span>Proteina:{data.nutritions.protein}</span>
+              <span>Açucar: {data.nutritions.sugar}</span>
+              </div>
+                <span className="Quantidade">Quantidade: {plus}</span>
+              <div className="buttons">
+
+              <button onClick={()=>{
+                setPlus(plus + 1)
+              }}>+</button>
+              <button onClick={()=>{
+                setPlus(plus -1)
+              }}>-</button>
+              </div>
+              <span className="valorTotal">Valor Total: $ {data.price * plus}</span>
+            </div>
+
+
+
+    </div>
+      : null}
+    </div>
+  )
+}
+
+export default Carrinho
