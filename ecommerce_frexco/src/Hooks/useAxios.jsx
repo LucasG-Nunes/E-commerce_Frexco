9e35252b2e71834a43b950261c54eb68f8ac4174
LucasG-Nunes <lucasgnam12@gmail.com>
2022-08-09 01:04:48 -0300

    :sparkles: Adicionando requisição com params para fruta individual

--- ecommerce_frexco/src/Hooks/useAxios.jsx
+++ ecommerce_frexco/src/Hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 export const useAxios = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState();
 
   useEffect(() => {
     const resData = async () => {
@@ -21,3 +21,30 @@ export const useAxios = () => {
   }, []);
   return { data };
 };
+
+
+export const useAxiosParams = (fruits) =>{
+  const [data, setData] = useState();
+  const id = fruits;
+
+  useEffect(() => {
+    const resData = async () => {
+      try {
+        await axios
+          .get(`http://localhost:3333/produtos/${id}`, {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+            },
+          })
+          .then((res) => setData(res.data));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    resData();
+  }, []);
+  return { data };
+
+}
+
+
