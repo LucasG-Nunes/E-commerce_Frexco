78e018bde2b749efc7de71f2096fea151ccd70e4
LucasG-Nunes <lucasgnam12@gmail.com>
2022-08-09 01:06:45 -0300

    :bulb: Adicionando Contextos para compartilhar estado globalmente na aplicação

--- ecommerce_frexco/src/index.js
+++ ecommerce_frexco/src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "./styles/Index.css"
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/Index.css";
+import App from "./App";
+import { CarrinhoContextProvider } from "./Context/CarrinhoContext";
+import { InputContextProvider } from "./Context/InputContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <InputContextProvider>
+      <CarrinhoContextProvider>
+        <App />
+      </CarrinhoContextProvider>
+    </InputContextProvider>
   </React.StrictMode>
 );
