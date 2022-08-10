1fd58c21c9a341cb874e2d274efc4399f8774bbc
LucasG-Nunes <lucasgnam12@gmail.com>
2022-08-10 09:51:13 -0300

    :construction: Adicionando melhores práticas html

--- ecommerce_frexco/src/Pages/Carrinho/Carrinho.jsx
+++ ecommerce_frexco/src/Pages/Carrinho/Carrinho.jsx
@@ -4,38 +4,47 @@ import { CarrinhoContext } from "../../Context/CarrinhoContext"
 import { useAxiosParams } from "../../Hooks/useAxios"
 import { useState } from "react"
 import Modal from "../../Components/ModalCompra/Modal"
+import ModalCEP from "../../Components/ModalCep/ModalCEP"
 
 const Carrinho = () => {
   const { fruit, setFruit } = useContext(CarrinhoContext);
   const { data } = useAxiosParams(fruit);
   const [plus, setPlus] = useState(1)
-  const [modal,setModal] = useState(false)
+  const [modal, setModal] = useState(false)
+  const [modalCEP,setModalCep] = useState(false)
   return (
     <div className="background">
       <Navbar />
+
       {!fruit && <p className="avisoVazio">Não há nada no seu carrinho!</p>}
+
       {data ?
-        <div className={"containerCarrinho"}>
+
+        <container className={"containerCarrinho"}>
           <div>
             <img src={data.image} alt="ImagemFruta" />
           </div>
-          <div className="Informations">
-            <div className="header">
+          <section className="Informations">
+
+            <article className="header">
               <h4>{data.name}</h4>
               <button onClick={() => {
                 setFruit('')
                 window.location.reload();
               }}>x</button>
-            </div>
-            <div className="nutritions">
+            </article>
+
+            <article className="nutritions">
               <span>Valor: ${data.price}</span>
               <span>Calorias: {data.nutritions.calories}</span>
               <span>Gordura: {data.nutritions.fat}</span>
               <span>Carboidrato: {data.nutritions.carbohydrates}</span>
               <span>Proteina:{data.nutritions.protein}</span>
               <span>Açucar: {data.nutritions.sugar}</span>
-            </div>
+            </article>
+
             <span className="Quantidade">Quantidade: {plus}</span>
+
             <div className="buttons">
 
               <button onClick={() => {
@@ -44,12 +53,33 @@ const Carrinho = () => {
               <button onClick={() => {
                 setPlus(plus - 1)
               }}>-</button>
+
             </div>
+
             <span className="valorTotal">Valor Total: $ {data.price * plus}</span>
-          {plus > 0 ? <button className="confirmarCompra" onClick={()=>{setModal(true)}}>Confirmar Compra</button>: null}
-          {modal ? <Modal setModal={setModal}/> : null}
-          </div>
-        </div>
+
+            {plus > 0 ?
+              <div className="buttonsConfirmarCompra">
+                <button onClick={() => { setModal(true) }}>Confirmar Compra</button>
+                <button onClick={()=> {setModalCep(true)}} >Simular Frete</button>
+              </div>
+              :
+              null
+            }
+
+            {modal ?
+              <Modal setModal={setModal} />
+              :
+              null}
+            {
+              modalCEP?
+              <ModalCEP setModalCep={setModalCep}/>
+              :
+              null
+            }
+              
+          </section>
+        </container>
         : null}
     </div>
   )
