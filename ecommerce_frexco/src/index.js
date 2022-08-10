0659ed1c3c7e5f518b64d4a30613bc49557c413a
LucasG-Nunes <lucasgnam12@gmail.com>
2022-08-10 10:13:54 -0300

    :package: Adicionando provider do contexto CepContext

--- ecommerce_frexco/src/index.js
+++ ecommerce_frexco/src/index.js
@@ -4,13 +4,16 @@ import "./styles/Index.css";
 import App from "./App";
 import { CarrinhoContextProvider } from "./Context/CarrinhoContext";
 import { InputContextProvider } from "./Context/InputContext";
+import { CepContextProvider } from "./Context/CepContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <InputContextProvider>
-      <CarrinhoContextProvider>
-        <App />
-      </CarrinhoContextProvider>
-    </InputContextProvider>
+    <CepContextProvider>
+      <InputContextProvider>
+        <CarrinhoContextProvider>
+          <App />
+        </CarrinhoContextProvider>
+      </InputContextProvider>
+    </CepContextProvider>
   </React.StrictMode>
 );
